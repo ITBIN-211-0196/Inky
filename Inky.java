6951180395488c544dd0e2791e0e9e47b3d49274
ITBIN-211-0196 <163078435+ITBIN-211-0196@users.noreply.github.com>
2024-03-12 12:48:02 +0530

    first commit

---
+++ Inky.java
@@ -0,0 +1,26 @@
+public class Inky{
+ 
+public Inky (String pinky)
+{
+   System.out.println(pinky);
+}
+
+public Inky(String pinky,String blinky,String rinky)
+{
+   System.out.println(blinky+"  " + pinky);
+System.out.println(pinky+"  " + rinky);
+System.out.println(blinky+"  " + pinky+"  "+rinky);
+}
+
+public static void main(String[] args)
+{
+   String pinky="donky";
+   String blinky="pinky";
+   String rinky="monkey";
+
+   Inky ponky = new Inky(pinky);
+   Inky ponky2= new Inky(pinky,blinky,rinky);
+}
+}
+
+  
